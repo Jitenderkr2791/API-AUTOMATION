d7f0f9ac8d033c34854833e017e4171bc66db2b5
Jitender.Kumar <jk885827@gmail.com>
2025-06-02 17:16:58 +0530

    update runner file

--- src/test/java/TestRunner/RunCucumberTest.java
+++ src/test/java/TestRunner/RunCucumberTest.java
@@ -4,27 +4,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith (Cucumber.class)
-@CucumberOptions
-	(
-	    tags= "@runthis",
+@CucumberOptions(features = "src/test/resources/feature/",
+		glue="stepdefinition",
 		
-	      features = {
-			//	      "classpath:feature/Post_login.feature",                                 
-	    //  	     "classpath:feature/Get_children_List_API.feature",
-		//		     "classpath:feature/Get_Enrollments.feature",
-	 	//          "classpath:feature/Post_Child_Information_Step1.feature",                         
-	 	 //           "classpath:feature/View_capacity_management.feature",
-		//		    "classpath:feature/Put_Enrollment_Status.feature",						
-	 	//          "classpath:feature/Post_Create_Classroom.feature",
-	    //		  "classpath:feature/Post_Mark_Absent_Graduate.feature"
-				 "classpath:feature/Happy_Path_Flow.feature"
-	    //		  "classpath:feature/enroll_Regular_Dropin.feature"		 	  
-	    },
-	    
-		
-		glue={"stepdefinition"},
-		dryRun = false,
-		monochrome=true,
+
+		//dryRun = false,
+		//monochrome=true,
 		plugin = {"pretty",
 			    "html:target/cucumber-reports.html",
 			    "json:target/cucumber.json", 
@@ -36,3 +21,14 @@ public class RunCucumberTest
 {
 
 }
+
+//	      "classpath:feature/Post_login.feature",
+//  	     "classpath:feature/Get_children_List_API.feature",
+//		     "classpath:feature/Get_Enrollments.feature",
+//          "classpath:feature/Post_Child_Information_Step1.feature",
+//           "classpath:feature/View_capacity_management.feature",
+//		    "classpath:feature/Put_Enrollment_Status.feature",
+//          "classpath:feature/Post_Create_Classroom.feature",
+//		  "classpath:feature/Post_Mark_Absent_Graduate.feature"
+//		  "classpath:feature/enroll_Regular_Dropin.feature"
+// "classpath:feature/Happy_Path_Flow.feature"
